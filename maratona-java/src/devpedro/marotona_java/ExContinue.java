5f901c2314666cffc07fb6b9f89e7e1ca816f574
PedroBrito7 <pedrobritofreitas10@gmail.com>
2025-11-20 23:12:23 -0300

    Fechado Introdução

--- maratona-java/src/devpedro/marotona_java/ExContinue.java
+++ maratona-java/src/devpedro/marotona_java/ExContinue.java
@@ -1,13 +1,13 @@
 package devpedro.marotona_java;
 
-public class ExBreak {
+public class ExContinue {
     // dado valor de um carro descubra quantas vezes ele pode ser parcelado
     public static void main(String[] args) {
         double valorTotal = 30000;
-        for (int installment = 1; installment <= valorTotal; installment++) {
+        for (int installment = (int) valorTotal; installment >= 1 ; installment--) {
             double valorInstallment = valorTotal / installment;
-            if (valorInstallment < 1000){
-                break;
+            if(valorInstallment < 1000){
+                continue;
             }
             System.out.println("Parcela " + installment + " R$ " + valorInstallment);
         }
