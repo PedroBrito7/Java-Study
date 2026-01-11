5b3828148faa7a9969b069e810e429b035cd6611
PedroBrito7 <pedrobritofreitas10@gmail.com>
2026-01-10 22:55:39 -0300

    feat: add OuterClassesTest01 with inner class demonstrating access to outer class attributes

--- maratona-java/src/javacore/ZZAclassesinternas/test/InnerClassTest01.java
+++
@@ -1,7 +0,0 @@
-package javacore.ZZAclassesinternas.test;
-
-public class InnerClassTest01 {
-    public static void main(String[] args) {
-// é uma classe dentro da outra classe, util quando nao é interfaces
-    }
-}
