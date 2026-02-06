df4aee730ed54b0068cedce5668fd602164356fd
PedroBrito7 <pedrobritofreitas10@gmail.com>
2026-02-05 22:08:06 -0300

    feat : update design pattern builder

---
+++ maratona-java/src/javacore/ZZHdesignpatterns/test/BuilderPatternTest01.java
@@ -0,0 +1,16 @@
+package javacore.ZZHdesignpatterns.test;
+
+import javacore.ZZHdesignpatterns.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        //new Person("Pedro","Brito", "Viradonojiraya", "pedrobrito@gmail.com");
+        // Construtor com mt argumento - Design pattern Builder = organiza isso
+       new Person.PersonBuilder()
+               .firstName("pedro")
+               .lastName("brito")
+               .username("pedro7brito")
+               .email("pedro.ff@gmail.com");
+
+           }
+}
