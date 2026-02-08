b0e7648d9df7126fd1d9fb70301dceba237a33f6
PedroBrito7 <pedrobritofreitas10@gmail.com>
2026-02-08 18:33:36 -0300

    feat : add ConnectionFactory class and docker-compose configuration for MySQL service

---
+++ maratona-java/src/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package javacore.ZZIjdbc.conn;
+
+public class ConnectionFactory {
+    // java.sql = Connection, Statement, ResultSet, DriverManger
+    // para cada banco vai ser uma interface padrao do banco
+    
+
+}
